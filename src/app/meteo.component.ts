d1b3283e108623e2f89755749f043eb48b8b50c0
anestola <alessandro.nestola@gmail.com>
2024-01-31 09:32:28 +0100

    init

--- src/app/meteo.component.ts
+++ src/app/meteo.component.ts
@@ -2,7 +2,7 @@ import {Component, effect, inject, input, signal} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../environments/environment.development";
 import {Meteo} from "./model/Meteo";
-import {DatePipe, JsonPipe} from "@angular/common";
+import {DatePipe, JsonPipe, NgOptimizedImage} from "@angular/common";
 import {MeteoCardComponent} from "./meteo-card.component";
 
 @Component({
@@ -11,26 +11,28 @@ import {MeteoCardComponent} from "./meteo-card.component";
   imports: [
     JsonPipe,
     DatePipe,
-    MeteoCardComponent
+    MeteoCardComponent,
+    NgOptimizedImage
   ],
   template: `
     <div class="md:container md:mx-auto p-10">
-      @if (loading){
+      @if (loading) {
         <div class="flex justify-center">
           <span class="loading loading-ring loading-lg"></span>
         </div>
       }
-      @if(!error() && !loading){
+      @if (!isError && !loading) {
         @if (meteo(); as meteo) {
-          <div class="border border-1 dark:border-slate-700 dark:bg-slate-700 dark:hover:bg-sky-700 hover:bg-gray-200 rounded-2xl w-full flex-wrap p-5 flex items-center justify-between shadow-[0_0px_60px_-15px_rgba(0,0,0,0.3)]">
-            <div class="flex flex-col items-center md:w-1/2 w-full  justify-center">
-              <p class="text-2xl font-bold text-center">{{meteo.location.name}}</p>
-              <p class="text-normal">{{meteo.location.region}} - {{meteo.location.country}}</p>
-              <p class="text-sm">lat: {{meteo.location.lat}}, lon: {{meteo.location.lon}}</p>
+          <div
+            class="border border-1 dark:border-slate-700 dark:bg-slate-700 dark:hover:bg-sky-700 hover:bg-gray-200 rounded-2xl w-full flex-wrap p-5 flex items-center justify-between shadow-[0_0px_60px_-15px_rgba(0,0,0,0.3)]">
+            <div class="flex flex-col items-center md:w-1/2 w-full justify-center">
+              <p class="text-2xl font-bold text-center">{{ meteo.location.name }}</p>
+              <p class="text-normal">{{ meteo.location.region }} - {{ meteo.location.country }}</p>
+              <p class="text-sm">lat: {{ meteo.location.lat }}, lon: {{ meteo.location.lon }}</p>
             </div>
-            <div class="flex items-center md:w-1/2 w-full justify-center">
-                <img [src]="meteo.current.condition.icon" [alt]="meteo.current.condition.text">
-                <span class="text-2xl font-bold">{{meteo.current.condition.text}}</span>
+            <div class="flex flex-wrap items-center md:w-1/2 w-full justify-center">
+              <img [ngSrc]="meteo.current.condition.icon" [alt]="meteo.current.condition.text" width="64" height="64">
+              <span class="text-2xl font-bold">{{ meteo.current.condition.text }}</span>
             </div>
           </div>
           <div class="border border-1 dark:border-sky-800 rounded-2xl mt-5 w-full flex flex-wrap p-5">
@@ -59,8 +61,9 @@ import {MeteoCardComponent} from "./meteo-card.component";
               <app-meteo-card [data]="meteo.current.precip_mm.toString()" addInformation="mm" label="Precipitazioni"/>
             </div>
             <div class="w-full sm:w-1/3 p-1">
-              <div class="flex flex-col justify-center items-center dark:bg-sky-950 dark:hover:bg-sky-800 bg-slate-100 hover:bg-slate-200 rounded p-5 w-full">
-                <span class="text-2xl font-bold h-full">{{meteo.current.last_updated | date : 'HH:mm'}}</span>
+              <div
+                class="flex flex-col justify-center items-center dark:bg-sky-950 dark:hover:bg-sky-800 bg-slate-100 hover:bg-slate-200 rounded p-5 w-full">
+                <span class="text-2xl font-bold h-full">{{ meteo.current.last_updated | date : 'HH:mm' }}</span>
                 <span class="text-sm">Ultimo aggiornamento</span>
               </div>
             </div>
@@ -69,21 +72,25 @@ import {MeteoCardComponent} from "./meteo-card.component";
             @for (fore of meteo.forecast.forecastday; track $index) {
               <div class="w-full sm:w-1/3 p-1">
                 <div class="flex flex-col items-center w-full justify-center rounded-2xl p-3">
-                  <img [src]="fore.day.condition.icon" [alt]="fore.day.condition.text"/>
-                  <span class="font-bold">{{fore.day.condition.text}}</span>
-                  <span class="text-sm">{{fore.date | date: 'dd/MM/yyyy'}}</span>
+                  <img [ngSrc]="fore.day.condition.icon" [alt]="fore.day.condition.text" height="64" width="64"/>
+                  <span class="font-bold">{{ fore.day.condition.text }}</span>
+                  <span class="text-sm">{{ fore.date | date: 'dd/MM/yyyy' }}</span>
                   <div class="flex gap-2">
                     <div class="flex">
-                      <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19V5m0 14-4-4m4 4 4-4"/>
+                      <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                           xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M12 19V5m0 14-4-4m4 4 4-4"/>
                       </svg>
-                      {{fore.day.mintemp_c}}°
+                      {{ fore.day.mintemp_c }}°
                     </div>
                     <div class="flex">
-                      <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v13m0-13 4 4m-4-4-4 4"/>
+                      <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                           xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M12 6v13m0-13 4 4m-4-4-4 4"/>
                       </svg>
-                      {{fore.day.maxtemp_c}}°
+                      {{ fore.day.maxtemp_c }}°
                     </div>
                   </div>
                 </div>
@@ -92,18 +99,22 @@ import {MeteoCardComponent} from "./meteo-card.component";
           </div>
         } @else {
           <div class="">
-            <div class="border border-1 dark:border-sky-800 rounded-2xl mt-5 w-full p-5 flex flex-col justify-center items-center gap-3 shadow-[0_0px_60px_-15px_rgba(0,0,0,0.3)]">
-              <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11h2v5m-2 0h4m-2.6-8.5h0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+            <div
+              class="border border-1 dark:border-sky-800 rounded-2xl mt-5 w-full p-5 flex flex-col justify-center items-center gap-3 shadow-[0_0px_60px_-15px_rgba(0,0,0,0.3)]">
+              <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                   fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M10 11h2v5m-2 0h4m-2.6-8.5h0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
               </svg>
               <span class="text-center">Inserisci il nome di una città!</span>
             </div>
           </div>
         }
       }
-      @if (error()){
-        <div class="bg-error rounded-2xl text-white p-5 text-center">
-          {{error()}}
+      @if (isError) {
+        <div class="rounded-2xl p-5 text-center shadow-2xl shadow-error dark:text-error text-black">
+          <p class="font-bold">Città non trovata!</p>
+          <p>controlla di averla scritta bene</p>
         </div>
       }
     </div>
@@ -113,7 +124,7 @@ import {MeteoCardComponent} from "./meteo-card.component";
 export class MeteoComponent {
   city = input.required<string>();
   meteo = signal<Meteo | null>(null)
-  error = signal<string | null>(null);
+  isError = false;
   loading = false;
   http = inject(HttpClient)
   constructor() {
@@ -123,16 +134,15 @@ export class MeteoComponent {
       this.http.get<Meteo>('https://api.weatherapi.com/v1/forecast.json?q=' + this.city() + '&days=3&lang=it&key=' + environment.apiKeyMeteo).subscribe({
         next: res => {
           this.meteo.set(res);
-          this.error.set(null);
+          this.isError = false;
           this.loading = false;
         },
-        error: err => {
+        error: () => {
           this.meteo.set(null)
-          this.error.set(err.error.error.message)
+          this.isError = true;
           this.loading = false;
         }
       })
-      console.log("richiamo endpoint con city ", this.city())
     });
   }
 }
